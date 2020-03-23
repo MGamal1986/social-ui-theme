8dc42a42f7c6d37bdc9195c5e0f4bbc1031195e4
MGamal1986 <elsayedmg@gmail.com>
2020-03-22 18:06:20 -0700

    fix gulp watch process for autoprefixer and babel

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,22 +1,40 @@
+"use strict";
+
+function _createForOfIteratorHelper(o) { if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) { if (Array.isArray(o) || (o = _unsupportedIterableToArray(o))) { var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var it, normalCompletion = true, didErr = false, err; return { s: function s() { it = o[Symbol.iterator](); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it["return"] != null) it["return"](); } finally { if (didErr) throw err; } } }; }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(n); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
 // select color button event
-let sel = document.querySelector('.color-select i');
-let aside = document.querySelector('.color-select');
-let homePage = document.querySelector('#home');
-let body = document.body;
+var sel = document.querySelector('.color-select i');
+var aside = document.querySelector('.color-select');
+var homePage = document.querySelector('#home');
+var body = document.body;
 sel.addEventListener('click', function (e) {
   aside.classList.toggle('hide');
 }); // add color change event
 
-let colors = document.querySelectorAll('.color-select li');
-colors.forEach(color => {
-  color.addEventListener('click', event => {
+var colors = document.querySelectorAll('.color-select li');
+colors.forEach(function (color) {
+  color.addEventListener('click', function (event) {
     body.dataset.color = event.target.dataset.color;
-    let listItems = event.target.parentElement.children; // loop throgth all list items and remove active class
+    var listItems = event.target.parentElement.children; // loop throgth all list items and remove active class
 
-    for (item of listItems) {
-      item.classList.remove('active');
-    } // add active class to clicked item
+    var _iterator = _createForOfIteratorHelper(listItems),
+        _step;
 
+    try {
+      for (_iterator.s(); !(_step = _iterator.n()).done;) {
+        item = _step.value;
+        item.classList.remove('active');
+      } // add active class to clicked item
+
+    } catch (err) {
+      _iterator.e(err);
+    } finally {
+      _iterator.f();
+    }
 
     event.target.classList.add('active');
   });
