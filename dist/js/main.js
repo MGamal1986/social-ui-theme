16577c964dbec352f626dd43aae25ad14c2580c7
MGamal1986 <elsayedmg@gmail.com>
2020-03-24 13:39:47 -0700

    after compile es6 to es5

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,54 +1,58 @@
+"use strict";
+
 // select color button event
-let sel = document.querySelector('.color-select i');
-let listItems = document.querySelectorAll('.color-select li');
-let aside = document.querySelector('.color-select');
-let homePage = document.querySelector('#home');
-let body = document.body;
+var sel = document.querySelector('.color-select i');
+var listItems = document.querySelectorAll('.color-select li');
+var aside = document.querySelector('.color-select');
+var homePage = document.querySelector('#home');
+var body = document.body;
 sel.addEventListener('click', function (e) {
   aside.classList.toggle('hide');
-});
-
-// add color change event
+}); // add color change event
 // localStorage.removeItem('color-select');
-let colStorage = localStorage['color-select'];
-if(colStorage){
-  if(colStorage === '#1AB6CF'){
+
+var colStorage = localStorage['color-select'];
+
+if (colStorage) {
+  if (colStorage === '#1AB6CF') {
     body.dataset.color = 'default';
     listItems[0].classList.add('active');
-  }else if(colStorage === '#2980b9'){
+  } else if (colStorage === '#2980b9') {
     body.dataset.color = 'color1';
     listItems[1].classList.add('active');
-  }else if(colStorage === '#27ae60'){
+  } else if (colStorage === '#27ae60') {
     body.dataset.color = 'color2';
     listItems[2].classList.add('active');
   }
 }
- let colors = document.querySelectorAll('.color-select li');
- colors.forEach((color)=>{
-    color.addEventListener('click',(event)=>{
-        // store col attribute of clicked item
-        let col = event.target.dataset.col;
-        // save color selection in localstorage
-        localStorage.setItem('color-select' , col);
-        if(col === '#1AB6CF'){
-          body.dataset.color = 'default';
-        }else if(col === '#2980b9'){
-          body.dataset.color = 'color1';
-        }else if(col === '#27ae60'){
-          body.dataset.color = 'color2';
-        }
-        
-        // loop throgth all list items and remove active class
-        for(item of listItems){
-          item.classList.remove('active');
-        }
-        // add active class to clicked item
-        event.target.classList.add('active');
-    })
- })
-//  add click event to body to close color selector feature
- body.addEventListener('click',(event)=>{
-  if(!event.target.classList.contains('color-select')  && !event.target.classList.contains('color-item') && !event.target.classList.contains('fa-cog')){
+
+var colors = document.querySelectorAll('.color-select li');
+colors.forEach(function (color) {
+  color.addEventListener('click', function (event) {
+    // store col attribute of clicked item
+    var col = event.target.dataset.col; // save color selection in localstorage
+
+    localStorage.setItem('color-select', col);
+
+    if (col === '#1AB6CF') {
+      body.dataset.color = 'default';
+    } else if (col === '#2980b9') {
+      body.dataset.color = 'color1';
+    } else if (col === '#27ae60') {
+      body.dataset.color = 'color2';
+    } // loop throgth all list items and remove active class
+
+
+    listItems.forEach(function (item) {
+      item.classList.remove('active');
+    }); // add active class to clicked item
+
+    event.target.classList.add('active');
+  });
+}); //  add click event to body to close color selector feature
+
+body.addEventListener('click', function (event) {
+  if (!event.target.classList.contains('color-select') && !event.target.classList.contains('color-item') && !event.target.classList.contains('fa-cog')) {
     aside.classList.add('hide');
   }
- });
+});
